c84478167123dbccc48969727744ed3bb8f15a7d
Brayan Silva <brayan_silva82151@elpoli.edu.co>
2022-02-17 22:03:20 -0500

    Autenticación con fb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD0Nao_k9TN5IoTO9NVl6-koLvcC3VCSSc',
+    authDomain: 'ingreso-egreso-be2e4.firebaseapp.com',
+    projectId: 'ingreso-egreso-be2e4',
+    storageBucket: 'ingreso-egreso-be2e4.appspot.com',
+    messagingSenderId: '520753158745',
+    appId: '1:520753158745:web:c7817ac00e41c0f499af48'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD0Nao_k9TN5IoTO9NVl6-koLvcC3VCSSc',
+    authDomain: 'ingreso-egreso-be2e4.firebaseapp.com',
+    projectId: 'ingreso-egreso-be2e4',
+    storageBucket: 'ingreso-egreso-be2e4.appspot.com',
+    messagingSenderId: '520753158745',
+    appId: '1:520753158745:web:c7817ac00e41c0f499af48'
+  }
 };
 
 /*
